8aaf908bab5762f51c81c9ce211faaa72ef412cc
NiranjanHegde26 <nirubs2609@gmail.com>
2024-08-03 19:24:49 +0200

    New files checked in

---
+++ analysis.r
@@ -0,0 +1,4 @@
+"
+Author: Niranjana Hegde BS
+Written on: 03/08/2024, Saarbruecken
+"
---
+++ visualizations.r
@@ -0,0 +1,7 @@
+"
+Author: Niranjana Hegde BS
+Written on: 03/08/2024, Saarbruecken
+Reference documentation from R: 
+    - https://rkabacoff.github.io/datavis/index.html
+    - https://ggplot2.tidyverse.org/
+"
